e953a7562fbb2b14eff6df49702ced4c676a2a61
Dahir Mohamed <dmoabdi98@gmail.com>
2021-12-15 19:47:34 +0000

    created all models and established relationships

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,17 +1,32 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-const dbName = process.env.DB_NAME;
-const dbUser = process.env.DB_USER;
-const dbPassword = process.env.DB_PASSWORD;
-const dbHost = process.env.DB_HOST;
+// const dbName = process.env.DB_NAME;
+// const dbUser = process.env.DB_USER;
+// const dbPassword = process.env.DB_PASSWORD;
+// const dbHost = process.env.DB_HOST;
 
-const dbOptions = {
-  host: dbHost,
-  dialect: 'mysql',
-  port: 3306,
-  logging: false,
-};
+// const dbOptions = {
+//   host: dbHost,
+//   dialect: 'mysql',
+//   port: 3306,
+//   logging: false,
+// };
 
-const sequelize = new Sequelize(dbName, dbUser, dbPassword, dbOptions);
+// const sequelize = new Sequelize(dbName, dbUser, dbPassword, dbOptions);
+
+const sequelize = new Sequelize(
+  // Database name
+  "ecommerce_db",
+  // User
+  "root",
+  // Password
+  "Password123!!",
+  {
+    // Database location
+    host: "localhost",
+    dialect: "mysql",
+    port: 3306,
+  }
+);
 
 module.exports = sequelize;
--- src/models/ProductTag.js
+++ src/models/ProductTag.js
@@ -1,20 +1,40 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
-ProductTag.init(
-  {
-    // define columns
+const schema = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
   },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
-  }
-);
+  productId: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: "product",
+      key: "id",
+    },
+  },
+  tagId: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: "tag",
+      key: "id",
+    },
+  },
+};
+
+const options = {
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: false,
+  modelName: "productTag",
+};
+
+ProductTag.init(schema, options);
 
 module.exports = ProductTag;
